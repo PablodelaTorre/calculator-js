79b645204cb330fd255f611d340ae219dac32ae8
Pablo <delatorre.pablodaniel@gmail.com>
2022-03-09 19:02:57 -0300

    Funciones numeros y operadores realizadas

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const aC = document.getElementById("ac")
 const del = document.getElementById("del")
+
 const divi = document.getElementById("divi")
+
 const siete = document.getElementById("siete")
 const ocho = document.getElementById("ocho")
 const nueve = document.getElementById("nueve")
+
 const por = document.getElementById("por")
+
 const cuatro = document.getElementById("cuatro")
 const cinco = document.getElementById("cinco")
 const seis = document.getElementById("seis")
+
 const mas = document.getElementById("mas")
+
 const uno = document.getElementById("uno")
 const dos = document.getElementById("dos")
 const tres = document.getElementById("tres")
+
 const menos = document.getElementById("menos")
+
 const deci = document.getElementById("deci")
 const cero = document.getElementById("cero")
 const igual = document.getElementById("igual")
@@ -31,6 +39,7 @@ function clickNumero(val){
         actual.textContent = num1
     }else{
         num2 = num2 + val
+        actual.textContent = num2
     }
 }
 
@@ -43,7 +52,43 @@ function clickOperador(val){
 mas.addEventListener("click", ()=>{
     clickOperador("+")
 })
+menos.addEventListener("click", ()=>{
+    clickOperador("-")
+})
+divi.addEventListener("click", ()=>{
+    clickOperador("/")
+})
+por.addEventListener("click", ()=>{
+    clickOperador("*")
+})
 
 cero.addEventListener("click", ()=>{
     clickNumero(0)
+})
+uno.addEventListener("click", ()=>{
+    clickNumero(1)
+})
+dos.addEventListener("click", ()=>{
+    clickNumero(2)
+})
+tres.addEventListener("click", ()=>{
+    clickNumero(3)
+})
+cuatro.addEventListener("click", ()=>{
+    clickNumero(4)
+})
+cinco.addEventListener("click", ()=>{
+    clickNumero(5)
+})
+seis.addEventListener("click", ()=>{
+    clickNumero(6)
+})
+siete.addEventListener("click", ()=>{
+    clickNumero(7)
+})
+ocho.addEventListener("click", ()=>{
+    clickNumero(8)
+})
+nueve.addEventListener("click", ()=>{
+    clickNumero(9)
 })
