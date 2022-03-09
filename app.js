2f85dbeb3c9614f35472ed97f25a8b22e7dcb1ae
Pablo <delatorre.pablodaniel@gmail.com>
2022-03-09 18:42:30 -0300

    punteros botones agregados

---
+++ app.js
@@ -0,0 +1,22 @@
+const aC = document.getElementById("ac")
+const del = document.getElementById("del")
+const divi = document.getElementById("divi")
+const siete = document.getElementById("siete")
+const ocho = document.getElementById("ocho")
+const nueve = document.getElementById("nueve")
+const por = document.getElementById("por")
+const cuatro = document.getElementById("cuatro")
+const cinco = document.getElementById("cinco")
+const seis = document.getElementById("seis")
+const mas = document.getElementById("mas")
+const uno = document.getElementById("uno")
+const dos = document.getElementById("dos")
+const tres = document.getElementById("tres")
+const menos = document.getElementById("menos")
+const deci = document.getElementById("deci")
+const cero = document.getElementById("cero")
+const igual = document.getElementById("igual")
+
+function clickNum() {
+    
+}
